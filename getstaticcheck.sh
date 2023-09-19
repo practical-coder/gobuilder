7e20b56a8ba51747eaef085f74ff3d5513bf4e65
Jacek Witkiewicz <jacek.witkiewicz@gmail.com>
2023-09-19 16:36:24 +0200

    Staticcheck: download latest github release sans jq

--- getstaticcheck.sh
+++ getstaticcheck.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -e
 set -o xtrace
-release_url='https://api.github.com/repos/dominikh/go-tools/releases/latest'
+release_url=https://api.github.com/repos/dominikh/go-tools/releases/latest
 tar_name=staticcheck_linux_amd64.tar.gz
-query="[.assets[] | select(.name | match(\"${tar_name}$\"))][0] | .browser_download_url"
-tar_url=$(curl -s "${release_url}" | jq -r "${query}")
-echo "$tar_url"
-wget "$tar_url"
+#query="[.assets[] | select(.name | match(\"${tar_name}$\"))][0] | .browser_download_url"
+tar_url=$(curl -s "${release_url}" | grep -o "https.*${tar_name}" | head -n 1)
+echo $tar_url
+wget $tar_url
 tar zxvf "$tar_name"
